b391e71989e937ea948825b5a73596425b9f00d3
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-07-17 01:17:19 +0530

    error

--- client/src/componentes/ProjectSection.jsx
+++ client/src/componentes/ProjectSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProjectCard from './ProjectCard'
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import { useRef } from 'react';
--- client/src/componentes/SkillSections.jsx
+++ client/src/componentes/SkillSections.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SkillCard from './SkillCard.jsx'
 import { useDispatch, useSelector } from 'react-redux';
 import { setSkills } from '../redux/slice/userSlice';
