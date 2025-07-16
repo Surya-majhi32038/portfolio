0dd4c47433c84c2f167eca9e43ff549726ba35a6
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-07-17 01:32:13 +0530

    console

--- client/src/componentes/SkillCard.jsx
+++ client/src/componentes/SkillCard.jsx
@@ -1,6 +1,6 @@
 
 function SkillCard({skill}) {
-    console.log("here in skill card",skill)
+    // console.log("here in skill card",skill)
   return (
     <div className='flex items-center rounded-xl lg:py-4 py-2 mb-3  justify-between border-2 px-4 backdrop-blur-lg'>
         <h3 className='lg:text-2xl text-base text-gray-200 font-bold'>{skill.skill}</h3>
