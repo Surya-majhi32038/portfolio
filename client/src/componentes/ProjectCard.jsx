3a4debd8eddb748adc872718cbab977c91f27672
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-07-20 12:07:47 +0530

    off the hover effect on phone size

--- client/src/componentes/ProjectCard.jsx
+++ client/src/componentes/ProjectCard.jsx
@@ -4,7 +4,8 @@ import { TbBuildingBroadcastTowerFilled } from "react-icons/tb";
 function ProjectCard({project}) {
   return (
     <div className='lg:p-4 p-1'>
-    <div className='lg:h-fit h-[440px]  select-none lg:w-[300px] w-[270px]  flex flex-col overflow-hidden border-opacity-60 hover:shadow-lg hover:lg:scale-110 cursor-grab hover:border-4 hover:border-gray-400 transition-all hover:z-10 z-0 delay-100 duration-300 rounded-2xl gap-3 lg:gap-1 border-2 border-gray-400 backdrop-blur-xl'>
+    <div className='lg:h-fit h-[440px] select-none lg:w-[300px] w-[270px] flex flex-col overflow-hidden border-opacity-60 lg:hover:shadow-lg lg:hover:scale-110 cursor-grab lg:hover:border-4 lg:hover:border-gray-400 transition-all lg:hover:z-10 z-0 delay-100 duration-300 rounded-2xl gap-3 lg:gap-1 border-2 border-gray-400 backdrop-blur-xl'>
+
         {/* picture of the project */}
         <div>
             <img src={project.img} alt="project over view pic" className='lg:h-[200px] h-[180px] w-[300px] lg:w-[400px]'/>
